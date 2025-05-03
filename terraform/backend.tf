280624fec0cef9774e13290b9c6d1147a3100efa
Mansi Jain <jainmansi668@gmail.com>
2025-05-03 22:22:17 +0530

    initial

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "file-metadata-extract"
-    key            = "lambda-project/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "MetadataTable"
-    encrypt        = true
-  }
-}
